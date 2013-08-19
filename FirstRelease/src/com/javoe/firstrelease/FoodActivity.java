5815ae7609e8ef9bc3ad6834500f76a87f957fa9
Oscar Pacheco <ofpach513@gmail.com>
2013-08-18 22:01:18 -0400

    Added third screen with navigation
    Three template screens with navigation among all three. Back buttons
    implemented.

--- FirstRelease/src/com/javoe/firstrelease/FoodActivity.java
+++ FirstRelease/src/com/javoe/firstrelease/FoodActivity.java
@@ -2,12 +2,23 @@ package com.javoe.firstrelease;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class FoodActivity extends Activity {
+public class FoodActivity extends Activity implements OnClickListener{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.food);
 		
 		getActionBar().hide();
+		
+		((Button)findViewById(R.id.btnFoodBack)).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		finish();
+		
 	}
 }
--- FirstRelease/src/com/javoe/firstrelease/RestaurantActivity.java
+++ FirstRelease/src/com/javoe/firstrelease/RestaurantActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RestaurantActivity extends Activity implements OnClickListener {
@@ -24,10 +25,15 @@ public class RestaurantActivity extends Activity implements OnClickListener {
 		((LinearLayout)findViewById(R.id.food8)).setOnClickListener(this);
 		((LinearLayout)findViewById(R.id.food9)).setOnClickListener(this);
 		((LinearLayout)findViewById(R.id.food10)).setOnClickListener(this);
+		
+		((Button)findViewById(R.id.btnRestaurantBack)).setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
-		startActivity(new Intent("android.intent.action.Food"));
+		if(v.getId() == R.id.btnRestaurantBack)
+			finish();
+		else
+			startActivity(new Intent("android.intent.action.Food"));
 	}
 }
